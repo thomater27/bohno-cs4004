3aecc7e0eeba1a25bfd6bb6ff008007185356f04
Bayan <b.nezamabad@gmail.com>
2022-11-15 13:47:21 +0000

    Did all the thingys

--- src/main/MeetingDate.java
+++ src/main/MeetingDate.java
@@ -1,11 +1,12 @@
-import javax.print.DocFlavor;
-import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Arrays;
 
 public class MeetingDate {
     private int day;
     private int month;
     private int year;
-    private String[] monthNames = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    private final String[] monthNames = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    private final int[] shortMonths = {4, 6, 9, 11};
 
     public MeetingDate(int day, int month, int year) {
         this.day = day;
@@ -13,31 +14,96 @@ public class MeetingDate {
         this.year = year;
     }
 
-    public MeetingDate correctDate() {
+    public void correctDate() {
+        LocalDate today = LocalDate.now();
+        //past dates are changed to current date
+        if (isPast()) {
+            day = today.getDayOfMonth();
+            month = today.getMonthValue();
+            year = today.getYear();
+        } else {
+            correctMonth();
+            correctDay();
+        }
+    }
 
+    public boolean isPast() {
+        LocalDate today = LocalDate.now();
+        if (year < today.getYear()) {
+            return true;
+        }
+        if (year == today.getYear() && month < today.getMonthValue()) {
+            return true;
+        }
+        if (year == today.getYear() && month == today.getMonthValue() && day < today.getDayOfMonth()) {
+            return true;
+        }
+        return false;
+    }
+
+    public void correctMonth() {
+        if (month < 1) {
+            month = 1;
+        } else if (month > 12) {
+            month = 12;
+        }
+    }
+
+    public void correctDay() {
+        if (day < 1) {
+            day = 1;
+        } else if (month == 2 && day > 28) {
+            if (year % 4 == 0) {
+                day = 29;
+            } else {
+                day = 28;
+            }
+        } else if (day > 30) {
+            boolean found = false;
+            for (int shortMonth : shortMonths) {
+                if (month == shortMonth) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found) {
+                day = 30;
+            } else {
+                day = 31;
+            }
+        }
     }
 
     public int getDay() {
-
+        return day;
     }
 
     public int getMonthValue() {
-
+        return month;
     }
 
     public String getMonthName() {
-
+        return monthNames[month-1];
     }
 
     public int getYear() {
-
+        return year;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        MeetingDate that = (MeetingDate) o;
+        return day == that.day && month == that.month && year == that.year;
+    }
+
+    @Override
     public String toString() {
-
-    }
-
-    public boolean validateDate() {
-
+        return String.format("%02d/%02d/%4d", day, month, year);
     }
 }
--- src/test/MeetingDateTest.java
+++ src/test/MeetingDateTest.java
@@ -1,6 +1,6 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,20 +11,24 @@ class MeetingDateTest {
     int thisYear = now.getYear();
     MeetingDate date = new MeetingDate(thisDay, thisMonth, thisYear);
     MeetingDate futureDate = new MeetingDate(10, 8, thisYear+1);
+    MeetingDate testDate;
 
     @Test
+    @DisplayName("Test 1: Gets day")
     void getDay() {
         assertEquals(thisDay, date.getDay());
         assertEquals(10, futureDate.getDay());
     }
 
     @Test
+    @DisplayName("Test 2: Gets month value")
     void getMonthValue() {
         assertEquals(thisMonth, date.getMonthValue());
         assertEquals(8, futureDate.getMonthValue());
     }
 
     @Test
+    @DisplayName("Test 3: Gets month name")
     void getMonthName() {
         assertEquals("August", futureDate.getMonthName());
         MeetingDate temp = new MeetingDate(1, 1, thisYear+1);
@@ -34,52 +38,124 @@ class MeetingDateTest {
     }
 
     @Test
+    @DisplayName("Test 4: Gets year")
     void getYear() {
         assertEquals(thisYear, date.getYear());
         assertEquals(thisYear+1, futureDate.getYear());
     }
 
     @Test
-    void validateDate() { //Bayan: testing if validateDate returns whether a date is valid or not under a variety of conditions
-        assertTrue(date.validateDate());
-        assertTrue(futureDate.validateDate());
-        MeetingDate invalidDate = new MeetingDate(0, 10, thisYear+1);
-        assertFalse(invalidDate.validateDate());
-        invalidDate = new MeetingDate(30, 2, thisYear+1);
-        assertFalse(invalidDate.validateDate());
-        invalidDate = new MeetingDate(32, 1, thisYear+1);
-        assertFalse(invalidDate.validateDate());
-        invalidDate = new MeetingDate(31, 0, thisYear+1);
-        assertFalse(invalidDate.validateDate());
-        invalidDate = new MeetingDate(30, 13, thisYear+1);
-        assertFalse(invalidDate.validateDate());
-        invalidDate = new MeetingDate(10, 10, 2000);
-        assertFalse(invalidDate.validateDate());
-        invalidDate = new MeetingDate(0, 0, 0);
-        assertFalse(invalidDate.validateDate());
-        invalidDate = new MeetingDate(12, 11, 2022);
-        assertFalse(invalidDate.validateDate());
+    @DisplayName("Test 5: Ensures correctDate doesn't change valid dates")
+    void correctDate() {
+        //Testing correct date with valid dates
+        testDate = new MeetingDate(30, 11, thisYear+1);
+        testDate.correctDate();
+        assertEquals(new MeetingDate(30, 11, thisYear+1), testDate);
+        testDate = new MeetingDate(28, 2, thisYear+1);
+        testDate.correctDate();
+        assertEquals(new MeetingDate(28, 2, thisYear+1), testDate);
+        testDate = new MeetingDate(31, 7, thisYear+1);
+        testDate.correctDate();
+        assertEquals(new MeetingDate(31, 7, thisYear+1), testDate);
     }
 
     @Test
-    void correctDate() {
-        assertEquals(date, date.correctDate());
-        assertEquals(futureDate, futureDate.correctDate());
-        MeetingDate testDate = new MeetingDate(0, 12, thisYear+1);
-        assertEquals(new MeetingDate(1, 12, thisYear+1), testDate.correctDate());
-        testDate = new MeetingDate(32, 1, thisYear+1);
-        assertEquals(new MeetingDate(31, 1, thisYear+1), testDate.correctDate());
-        testDate = new MeetingDate(29, 2, 2071);
-        assertEquals(new MeetingDate(28, 2, 2071), testDate.correctDate());
-        testDate = new MeetingDate(29, 2, thisYear+4 - thisYear%4);
-        assertEquals(testDate, testDate.correctDate());
-        testDate = new MeetingDate(10, 0, thisYear+1);
-        assertEquals(new MeetingDate(10, 1, thisYear+1), testDate.correctDate());
+    @DisplayName("Test 6: Checks if correctDay works across the input space")
+    void correctDay() {
+        //Testing out-of-bounds day values
+        testDate = new MeetingDate(0, 12, thisYear + 1);
+        testDate.correctDay();
+        assertEquals(1, testDate.getDay());
+        testDate = new MeetingDate(32, 1, thisYear + 1);
+        testDate.correctDay();
+        assertEquals(31, testDate.getDay());
+        testDate = new MeetingDate(31, 4, thisYear + 1);
+        testDate.correctDay();
+        assertEquals(30, testDate.getDay());
+        testDate = new MeetingDate(29, 2, 3171);
+        testDate.correctDay();
+        assertEquals(28, testDate.getDay());
+        //Testing leap year exceptional circumstance
+        testDate = new MeetingDate(29, 2, thisYear + 4 - thisYear % 4);
+        testDate.correctDay();
+        assertEquals(29, testDate.getDay());
+        testDate = new MeetingDate(30, 2, thisYear + 4 - thisYear % 4);
+        testDate.correctDay();
+        assertEquals(29, testDate.getDay());
+        //Testing in bound values
+        testDate = new MeetingDate(1, 12, thisYear + 1);
+        testDate.correctDay();
+        assertEquals(1, testDate.getDay());
+        testDate = new MeetingDate(30, 4, thisYear + 1);
+        testDate.correctDay();
+        assertEquals(30, testDate.getDay());
+        testDate = new MeetingDate(31, 5, thisYear + 1);
+        testDate.correctDay();
+        assertEquals(31, testDate.getDay());
+        testDate = new MeetingDate(28, 2, thisYear + 1);
+        testDate.correctDay();
+        assertEquals(28, testDate.getDay());
+    }
+
+    @Test
+    @DisplayName("Test 7: Checks if correctMonth works across the input space")
+    void correctMonth() {
+        //Testing out-of-bounds month values
+        testDate = new MeetingDate(10, 0, thisYear + 1);
+        testDate.correctMonth();
+        assertEquals(1, testDate.getMonthValue());
         testDate = new MeetingDate(31, 13, thisYear);
-        assertEquals(new MeetingDate(31, 12, thisYear), testDate.correctDate());
+        testDate.correctMonth();
+        assertEquals(12, testDate.getMonthValue());
+        //Testing in bound month values
+        testDate = new MeetingDate(1, 1, thisYear + 1);
+        testDate.correctMonth();
+        assertEquals(1, testDate.getMonthValue());
+        testDate = new MeetingDate(1, 12, thisYear + 1);
+        testDate.correctMonth();
+        assertEquals(12, testDate.getMonthValue());
+    }
+
+    @Test
+    @DisplayName("Test 8: Checks if isPast returns correct values for past and future dates")
+    void isPast() {
+        //Testing if isPast returns correct value
+        testDate = new MeetingDate(31, 12, 2021);
+        assertTrue(testDate.isPast());
+        testDate = new MeetingDate(31, 10, 2022);
+        assertTrue(testDate.isPast());
+        testDate = new MeetingDate(1, 11, 2022);
+        assertTrue(testDate.isPast());
+        testDate = new MeetingDate(31, 12, thisYear);
+        assertFalse(testDate.isPast());
+        testDate = new MeetingDate(1, 1, thisYear+1);
+        assertFalse(testDate.isPast());
+        testDate = new MeetingDate(31, 1, thisYear+1);
+        assertFalse(testDate.isPast());
+    }
+
+    @Test
+    @DisplayName("Test 9: Checks if correctDate makes past dates today")
+    void correctDatePastDates() {
+        //Testing if past dates return current date
         testDate = new MeetingDate(25, 1, 2022);
+        testDate.correctDate();
         assertEquals(date, testDate);
         testDate = new MeetingDate(30, 4, 2020);
+        testDate.correctDate();
         assertEquals(date, testDate);
+        testDate = new MeetingDate(27, 8, 999);
+        testDate.correctDate();
+        assertEquals(date, testDate);
+    }
+
+    @Test
+    @DisplayName("Test 10: Checks if toString formats correctly")
+    void toStringTest() {
+        //Testing to see if toString formats correctly
+        testDate = new MeetingDate(1, 9, thisYear+1);
+        assertEquals(String.format("01/09/" + (thisYear+1)), testDate.toString());
+        testDate = new MeetingDate(26, 12, thisYear+1);
+        assertEquals(String.format("26/12/" + (thisYear+1)), testDate.toString());
     }
 }
