6e1645cd37844e36d960547d4167403fec054d15
carthy27 <98388152+carthy27@users.noreply.github.com>
2022-11-15 14:29:26 +0000

    made meeting time, meeting

---
+++ src/main/MeetingTime.java
@@ -0,0 +1,17 @@
+public class MeetingTime
+{ 
+   private String time;
+    
+    public MeetingTime(String h){
+      this.time = h;
+    }
+
+    
+    public String toString() {
+        
+        return time;
+    }
+
+    
+   
+}
